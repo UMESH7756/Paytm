d3447be1559517e0ce1d5df922d37b30ed72ba2c
Endless <Endless@LAPTOP-SPROEP6A>
2022-05-31 16:17:29 +0530

    create integer into test class

--- PAYTM/src/test/java/PAY/testclassPTM.java
+++ PAYTM/src/test/java/PAY/testclassPTM.java
@@ -74,7 +74,8 @@ public static void main(String[] args) throws InterruptedException {
 //	 Thread.sleep(3000);        
 //	 stockpage.clickOnMutualfundTab1(driver);
 	
-	 int xyza;
+	 int xyz;
+	 int d;
 	
 }
 }
